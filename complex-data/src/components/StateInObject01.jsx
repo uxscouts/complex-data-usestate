2c16f5da45fb5bbc0db0d9ead0e6a029638eee07
UX SCOUTS <admin@uxscouts.com>
2026-04-09 18:46:14 +0000

    saftey save

--- complex-data/src/components/StateInObject01.jsx
+++ complex-data/src/components/StateInObject01.jsx
@@ -19,7 +19,13 @@ function updateGreeting(){
     // The correct way to update the 
     // state object in React when using useState
 
+const [greeting2, setGreeting2] = useState({greet: "Hello World 2"});
 
+function updateGreeting2(){
+    const newGreeting2={...greeting2}
+    newGreeting2.greet="Hello World Wide Web 2";
+    setGreeting2(newGreeting2);
+}
 
     return (
         <>
@@ -28,6 +34,10 @@ function updateGreeting(){
             and updating it based on user-generated events</p>
             <p><span className="green">{greeting.greet}</span></p>
             <p><button onClick={updateGreeting}>Update Greeting</button></p>
+            <hr/>
+             <p><span className="green">{greeting2.greet}</span></p>
+            <p><button onClick={updateGreeting2}>Update Greeting</button></p>           
+
          </>   
     )
 }
